302253435f6243433460224d24b515c7e932f6f8
Chip Newman <chip594@gmail.com>
2019-09-01 23:42:23 -0500

    rename tab3 -> home

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -28,12 +28,12 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab3',
+        path: 'home',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../home/home.module').then(m => m.Tab3PageModule)
           }
         ]
       },
