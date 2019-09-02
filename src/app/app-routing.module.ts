bcbcf31226d8dee13c6d0ca99a582d70359af133
Chip Newman <chip594@gmail.com>
2019-09-02 16:10:19 -0500

    add routing for new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'projects', loadChildren: 'projects/projects.module#ProjectsPageModule' },
+  { path: 'donate', loadChildren: 'donate/donate.module#DonatePageModule' },
+  { path: 'fundraisers', loadChildren: 'fundraisers/fundraisers.module#FundraisersPageModule' },
+  { path: 'who-we-are', loadChildren: 'who-we-are/who-we-are.module#WhoWeArePageModule' },
+  { path: 'sponsors', loadChildren: 'sponsors/sponsors.module#SponsorsPageModule' }
 ];
 @NgModule({
   imports: [
