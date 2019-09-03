fce0c8d8379756691fd3fb28ffc7bc503616f558
Chip Newman <chip594@gmail.com>
2019-09-02 22:44:32 -0500

    added bottom buttons to projects page

--- src/app/projects/projects.page.ts
+++ src/app/projects/projects.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+import { HomePage } from '../home/home.page';
+
 
 @Component({
   selector: 'app-projects',
@@ -7,6 +11,32 @@ import { Component } from '@angular/core';
 })
 export class ProjectsPage {
 
-  constructor() { }
+    homePage = HomePage;
+
+    constructor(public navCtrl: NavController) { }
+
+    backToHomePage() {
+        this.navCtrl.navigateForward('/tabs');
+    }
+
+    goToMyJasPage() {
+        this.navCtrl.navigateForward('my-jas');
+    }
+
+    goToEventsPage() {
+        this.navCtrl.navigateForward('events');
+    }
+
+    goToHomePage() {
+        this.navCtrl.navigateForward('home');
+    }
+
+    goToNajaPage() {
+        this.navCtrl.navigateForward('naja');
+    }
+
+    goToInfoPage() {
+        this.navCtrl.navigateForward('info');
+    }
 
 }
