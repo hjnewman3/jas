6c3e6e5dae739e421997ea67af84fb40dfb801dd
Chip Newman <chip594@gmail.com>
2019-09-02 22:44:58 -0500

    added grid for home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+import { ProjectsPage } from '../projects/projects.page';
+import { EventsPage } from '../events/events.page';
+import { FundraisersPage } from '../fundraisers/fundraisers.page';
+import { DonatePage } from '../donate/donate.page';
+import { SponsorsPage } from '../sponsors/sponsors.page';
+import { WhoWeArePage } from '../who-we-are/who-we-are.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +15,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+    projectsPage = ProjectsPage;
+    eventsPage = EventsPage;
+    fundraisersPage = FundraisersPage;
+    donatePage = DonatePage;
+    sponsorsPage = SponsorsPage;
+    whoWeArePage = WhoWeArePage;
+
+  constructor(public navCtrl: NavController) { }
+
+    goToProjectsPage() {
+        this.navCtrl.navigateForward('/projects');
+    }
+
+    goToEventsPage() {
+        this.navCtrl.navigateForward('events');
+    }
+
+    goToFundraisersPage() {
+        this.navCtrl.navigateForward('/fundraisers');
+    }
+
+    goToDonatePage() {
+        this.navCtrl.navigateForward('/donate');
+    }
+
+    goToSponsorsPage() {
+        this.navCtrl.navigateForward('/sponsors');
+    }
+
+    goToWhoWeArePage() {
+        this.navCtrl.navigateForward('/who-we-are');
+    }
 
 }
