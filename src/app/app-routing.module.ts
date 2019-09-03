8489ccbeb1105b730024de0e59577921c15472f7
Chip Newman <chip594@gmail.com>
2019-09-02 22:46:18 -0500

    added routing for all non-tab pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,16 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'projects', loadChildren: 'projects/projects.module#ProjectsPageModule' },
-  { path: 'donate', loadChildren: 'donate/donate.module#DonatePageModule' },
-  { path: 'fundraisers', loadChildren: 'fundraisers/fundraisers.module#FundraisersPageModule' },
-  { path: 'who-we-are', loadChildren: 'who-we-are/who-we-are.module#WhoWeArePageModule' },
-  { path: 'sponsors', loadChildren: 'sponsors/sponsors.module#SponsorsPageModule' }
+  { path: 'my-jas', loadChildren: './my-jas/my-jas.module#MyJasPageModule'},
+  { path: 'home', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'naja', loadChildren: './naja/naja.module#NajaPageModule'},
+  { path: 'info', loadChildren: './info/info.module#InfoPageModule'},
+  { path: 'projects', loadChildren: './projects/projects.module#ProjectsPageModule' },
+  { path: 'events', loadChildren: './events/events.module#EventsPageModule'},
+  { path: 'donate', loadChildren: './donate/donate.module#DonatePageModule' },
+  { path: 'fundraisers', loadChildren: './fundraisers/fundraisers.module#FundraisersPageModule' },
+  { path: 'who-we-are', loadChildren: './who-we-are/who-we-are.module#WhoWeArePageModule' },
+  { path: 'sponsors', loadChildren: './sponsors/sponsors.module#SponsorsPageModule' }
 ];
 @NgModule({
   imports: [
